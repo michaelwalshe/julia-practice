49bc34747244cc85de0093e8f1c837ffb821ac7a
Michael Walshe <62556482+michaelwalshe@users.noreply.github.com>
2022-05-27 12:12:00 +0100

    Update triples

--- pythagorean-triplet/pythagorean-triplet.jl
+++ pythagorean-triplet/pythagorean-triplet.jl
@@ -1,14 +1,17 @@
 function pythagorean_triplets(n::Integer)
     triples = []
     for a in 1:n ÷ 3
-        # Formula from a^2 + b^2 = c^2 & a + b + c = N
-        c = (n^2 - 2*a*n) / (2n -2a)
-        b = n - a - c
+        # Formula derived from a^2 + b^2 = c^2 & a + b + c = N
+        b = (n^2 - 2*a*n) / (2n -2a)
+        c = n - a - b
         t = [a, b, c]
-        if t == floor.(t)
-            push!(triples, Tuple(sort(t)))
+        if (
+            t == floor.(t)  # Integer solns to the derived formula
+            && issorted(t)  # a < b < c
+        )
+            push!(triples, Tuple(t))
         end
     end
 
-    return unique(triples)
+    return triples
 end
